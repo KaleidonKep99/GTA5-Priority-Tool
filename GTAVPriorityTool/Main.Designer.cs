dd42790be94cbd066e9402514446f613838bb28b
Riccardo Loi <kaleidonkep99@outlook.com>
2017-09-03 09:24:16 +0200

    1.0

---
+++ GTAVPriorityTool/Main.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GTAVPriorityTool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Instruction = new System.Windows.Forms.Label();
+            this.Priority = new System.Windows.Forms.ComboBox();
+            this.ConfirmBtn = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.SetGTAVPriority = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // Instruction
+            // 
+            this.Instruction.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Instruction.Location = new System.Drawing.Point(0, 0);
+            this.Instruction.Name = "Instruction";
+            this.Instruction.Size = new System.Drawing.Size(333, 23);
+            this.Instruction.TabIndex = 0;
+            this.Instruction.Text = "Which level of priority do you want it to use?";
+            this.Instruction.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Priority
+            // 
+            this.Priority.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Priority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Priority.FormattingEnabled = true;
+            this.Priority.Items.AddRange(new object[] {
+            "Idle",
+            "Below normal",
+            "Normal",
+            "Above normal",
+            "High",
+            "Real-Time"});
+            this.Priority.Location = new System.Drawing.Point(12, 25);
+            this.Priority.Name = "Priority";
+            this.Priority.Size = new System.Drawing.Size(309, 21);
+            this.Priority.TabIndex = 1;
+            this.Priority.SelectedIndexChanged += new System.EventHandler(this.Priority_SelectedIndexChanged);
+            // 
+            // ConfirmBtn
+            // 
+            this.ConfirmBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConfirmBtn.Location = new System.Drawing.Point(246, 62);
+            this.ConfirmBtn.Name = "ConfirmBtn";
+            this.ConfirmBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmBtn.TabIndex = 2;
+            this.ConfirmBtn.Text = "OK";
+            this.ConfirmBtn.UseVisualStyleBackColor = true;
+            this.ConfirmBtn.Click += new System.EventHandler(this.ConfirmBtn_Click);
+            // 
+            // Status
+            // 
+            this.Status.Location = new System.Drawing.Point(9, 54);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(231, 34);
+            this.Status.TabIndex = 3;
+            this.Status.Text = "Status:\r\nIdle";
+            this.Status.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // SetGTAVPriority
+            // 
+            this.SetGTAVPriority.DoWork += new System.ComponentModel.DoWorkEventHandler(this.SetGTAVPriority_DoWork);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 97);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.ConfirmBtn);
+            this.Controls.Add(this.Priority);
+            this.Controls.Add(this.Instruction);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GTA V Priority Tool";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Instruction;
+        private System.Windows.Forms.ComboBox Priority;
+        private System.Windows.Forms.Button ConfirmBtn;
+        private System.Windows.Forms.Label Status;
+        private System.ComponentModel.BackgroundWorker SetGTAVPriority;
+    }
+}
+
---
+++ GTAVPriorityTool/Main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace GTAVPriorityTool
+{
+    public partial class Main : Form
+    {
+        [DllImport("user32")]
+        public static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32")]
+        public static extern bool EnableMenuItem(IntPtr hMenu, uint itemId, uint uEnable);
+
+        ProcessPriorityClass[] Priorities = new ProcessPriorityClass[6]
+        {
+            ProcessPriorityClass.Idle,
+            ProcessPriorityClass.BelowNormal,
+            ProcessPriorityClass.Normal,
+            ProcessPriorityClass.AboveNormal,
+            ProcessPriorityClass.High,
+            ProcessPriorityClass.RealTime
+        };
+        Int32 IndexOfPriority = 3;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Priority.SelectedIndex = IndexOfPriority;
+        }
+
+        private void Priority_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            IndexOfPriority = Priority.SelectedIndex;
+        }
+
+        private void ConfirmBtn_Click(object sender, EventArgs e)
+        {
+            Priority.Enabled = false;
+            ConfirmBtn.Enabled = false;
+            DisableCloseButton();
+            SetGTAVPriority.RunWorkerAsync();
+        }
+
+        private void SetGTAVPriority_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName("GTA5");
+
+                while (true)
+                {
+                    if (processes.Length == 0)
+                    {
+                        this.Invoke((MethodInvoker)delegate { Status.Text = "Status:\nWaiting for GTA V to start..."; });
+
+                        processes = Process.GetProcessesByName("GTA5");
+                        System.Threading.Thread.Sleep(10);
+                    }
+                    else
+                    {
+                        foreach (Process proc in processes) proc.PriorityClass = Priorities[IndexOfPriority];
+
+                        this.Invoke((MethodInvoker)delegate { Status.Text = String.Format("Status:\nGTA V has been set to {0} priority.", Priority.Text); });
+                        System.Threading.Thread.Sleep(500);
+                        this.Invoke((MethodInvoker)delegate { Status.Text = "Status:\nClosing..."; });
+                        System.Threading.Thread.Sleep(100);
+
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            catch { Environment.Exit(-1); }
+        }
+
+        private void DisableCloseButton()
+        {
+            EnableMenuItem(GetSystemMenu(this.Handle, false), 0xF060, 1);
+        }
+
+        private void EnableCloseButton()
+        {
+            EnableMenuItem(GetSystemMenu(this.Handle, false), 0xF060, 0);
+        }
+    }
+}
